f9f53ac7dc9f1622168680cc6339a0ef2dd91828
kishoreahhhhhh <kiii008kiii008@gmail.com>
2025-01-25 12:57:20 +0530

    Add files via upload

---
+++ Oddeven.java
@@ -0,0 +1,15 @@
+class Oddeven
+{
+ public static void main(String args[])
+  {
+    int num = 5;
+    if(num % 2 == 0)
+     {
+      System.out.println("the given number"+ num +" is even");
+     }
+   else
+     {
+     System.out.print("the given number "+num+" is odd");
+     }
+   }
+}
